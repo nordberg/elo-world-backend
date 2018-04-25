640a38548e3dffb050cad6ac1f09baac07742fd8
Andre Nyström <andre.nystrom@trioptima.com>
2018-04-25 19:43:41 +0200

    Working elo calculation

--- routes/elo.py
+++ routes/elo.py
@@ -1,16 +1,14 @@
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
-from enum import Enum
 
 
 blueprint = Blueprint('elo', __name__)
 db = SQLAlchemy()
-ELO_K = 32
 
-class MatchResult(Enum):
-    WIN=1
-    LOSE=0
-    DRAW=0.5
+ELO_K = 32
+WIN=1
+LOSE=0
+DRAW=0.5
 
 
 class Elo(db.Model):
@@ -39,18 +37,18 @@ def hello_world(sport, user):
 def hello_world():
     return 'Hello, elo!'
 
-def calculate_new_elo(old_elo1, old_elo2, s1):
+def calculate_new_elo(r1, r2, s1):
     """ Calculates new elo for two players
-    old_elo1 - elo of player 1
-    old_elo2 - elo of player 2
+    r1 - elo of player 1
+    r2 - elo of player 2
     s1 - match result from player one's perspective
         1 = win 0.5 = draw 0 = lose
     """
     s2 = abs(1-s1)
-    r1 = 10**(old_elo1/400)
-    r2 = 10**(old_elo2/400)
-    e1 = r1 / (r1 + r2)
-    e2 = r2 / (r1 + r2)
-    new_elo_1 = r1 + ELO_K*(s1 - e1)
-    new_elo_2 = r2 + ELO_K*(s2 - e2)
+    R1 = 10**(r1/400)
+    R2 = 10**(r2/400)
+    e1 = R1 / (R1 + R2)
+    e2 = R2 / (R1 + R2)
+    new_elo_1 = round(r1 + ELO_K*(s1 - e1))
+    new_elo_2 = round(r2 + ELO_K*(s2 - e2))
     return new_elo_1, new_elo_2
