95fd911fda691251e89ea1983c1ebc84e77fc45d
Marcus Nordberg <nordbergaren93@gmail.com>
2018-04-25 17:51:59 +0200

    Add match route

--- routes/matches.py
+++ routes/matches.py
@@ -6,8 +6,20 @@ blueprint = Blueprint('matches', __name__)
 
 
 @blueprint.route('/')
-def hello_world():
-    return 'Hello, matches!'
+def matches(id):
+    match = Match.query.filter_by(id=id).first()
+    if request.method == 'GET':
+        response = jsonify({
+            'id': match.id,
+            'team_1': match.team_1,
+            'team_2': match.team_2,
+            'score_1': match.score_1,
+            'score_2': match.score_2,
+            'date': match.date,
+            'sport': match.sport,
+        })
+
+        return response
 
 db = SQLAlchemy()
 
