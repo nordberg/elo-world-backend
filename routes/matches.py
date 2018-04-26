dde96f64a3d9604c8c2891e5258cdd10353bf028
Marcus Nordberg <nordbergaren93@gmail.com>
2018-04-26 13:46:33 +0200

    Add entrypoint for match id

--- routes/matches.py
+++ routes/matches.py
@@ -1,21 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from database import Match, Session, User, Sport
 
 
 blueprint = Blueprint('matches', __name__)
 
 
-@blueprint.route('/<id>/')
-def matches(id):
-    match = Match.query.filter_by(id=id).first()
-    if request.method == 'GET':
+@blueprint.route('/<match_id>/')
+def matches(match_id):
+    session = Session()
+    match = session.query(Match).get(match_id)
+    if request.method == 'GET' and match:
         response = jsonify({
             'id': match.id,
-            'team_1': match.team_1,
-            'team_2': match.team_2,
+            'team_1': session.query(User).get(match.team_1).name,
+            'team_2': session.query(User).get(match.team_2).name,
             'score_1': match.score_1,
             'score_2': match.score_2,
             'date': match.date,
-            'sport': match.sport,
+            'sport': session.query(Sport).get(match.sport).name,
         })
 
-        return response
+    else:
+        response = jsonify({'error': 'No match with that id'})
+    return response
