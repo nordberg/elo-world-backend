2ac30db314e1ff5b1e88499c14da63789b52bab3
Marcus Nordberg <nordbergaren93@gmail.com>
2018-04-26 14:32:52 +0200

    Add migrate things

--- migrate.py
+++ migrate.py
@@ -1,4 +1,4 @@
-from database import Sport, engine, Session, Base, User, Match
+from database import Sport, engine, Session, Base, User, Match, Elo
 from sqlalchemy import create_engine
 from database import Sport
 from datetime import datetime
@@ -18,13 +18,18 @@ session.add(eight_ball)
 user_1 = User(name='Marcus')
 user_2 = User(name='Dexter')
 
+
 session.add(user_1)
 session.add(user_2)
 
 session.commit()
 
+elo_1 = Elo(user=user_1.id, sport=table_tennis.id, score=1000)
+elo_2 = Elo(user=user_2.id, sport=table_tennis.id, score=1000)
 match = Match(team_1=user_1.id, team_2=user_2.id, score_1=2, score_2=1, date=datetime.now(), sport=table_tennis.id)
 
 session.add(match)
+session.add(elo_1)
+session.add(elo_2)
 
 session.commit()
