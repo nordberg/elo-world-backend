de9bc98588a53a3711aa0f8ef70b7249e635e488
Marcus Nordberg <nordbergaren93@gmail.com>
2018-04-26 14:10:31 +0200

    Add add_match endpoint

--- routes/matches.py
+++ routes/matches.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from database import Match, Session, User, Sport
+from datetime import datetime
 
 
 blueprint = Blueprint('matches', __name__)
@@ -23,3 +24,29 @@ def matches(match_id):
     else:
         response = jsonify({'error': 'No match with that id'})
     return response
+
+
+@blueprint.route('/add_match/', methods=['POST', 'PUT'])
+def add_match():
+    session = Session()
+    req = request.json
+    print(req)
+    team_1 = session.query(User).filter_by(name=req['team_1']).first()
+    team_2 = session.query(User).filter_by(name=req['team_2']).first()
+    sport = session.query(Sport).filter_by(name=req['sport']).first()
+    date = datetime.strptime(req['date'], '%Y-%m-%d').date()
+
+    match = Match(team_1=team_1.id, team_2=team_2.id, sport=sport.id, date=date, score_1=req['score_1'], score_2=req['score_2'])
+
+    session.add(match)
+    session.commit()
+
+    return jsonify({
+        'id': match.id,
+        'team_1': req['team_1'],
+        'team_2': req['team_2'],
+        'score_1': req['score_1'],
+        'score_2': req['score_2'],
+        'sport': req['sport'],
+        'date': req['date'],
+    })
