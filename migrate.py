c996845f843abc99798eb6738ce95eb4f40de691
Marcus Nordberg <nordbergaren93@gmail.com>
2018-04-26 12:00:22 +0200

    Add migration for pingis and biljard

--- migrate.py
+++ migrate.py
@@ -1,6 +1,8 @@
 import os
 import database
 from sqlalchemy import create_engine
+from database import Sport
+from sqlalchemy.orm import sessionmaker
 
 user = os.environ['POSTGRES_USER']
 password = os.environ['POSTGRES_PASSWORD']
@@ -10,4 +12,16 @@ engine = create_engine(
     "postgresql://{}:{}@db:5432/{}".format(user, password, db_name)
 )
 
+database.Base.metadata.drop_all(engine)
 database.Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+pingis = Sport(name='Pingis')
+eight_ball = Sport(name='Biljard')
+
+session.add(pingis)
+session.add(eight_ball)
+
+session.commit()
